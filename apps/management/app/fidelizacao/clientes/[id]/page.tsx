2dfbb93dd3cfe46bc1e42dc1e6f4afe37aead9b9
Bzuzinho <ricardo.jgvf@gmail.com>
2026-07-30 11:58:09 +0100

    feat(management): add loyalty customer detail route

---
+++ apps/management/app/fidelizacao/clientes/[id]/page.tsx
@@ -0,0 +1,6 @@
+import { LoyaltyCustomerDetail } from '../../../../components/loyalty-detail';
+
+export default async function LoyaltyCustomerPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  return <LoyaltyCustomerDetail userId={id} />;
+}
