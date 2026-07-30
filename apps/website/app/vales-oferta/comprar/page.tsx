dcd790234403e752a2903b63e2b454a6c3529e25
Bzuzinho <ricardo.jgvf@gmail.com>
2026-07-30 12:46:18 +0100

    feat(website): add gift card purchase route

---
+++ apps/website/app/vales-oferta/comprar/page.tsx
@@ -0,0 +1,9 @@
+import { GiftCardPurchaseForm } from '../../../components/gift-card-purchase-form';
+
+export default function GiftCardPurchasePage() {
+  return (
+    <main id="conteudo" className="account-page">
+      <GiftCardPurchaseForm />
+    </main>
+  );
+}
