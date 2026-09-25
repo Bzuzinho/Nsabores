2daaccb26de23351f62ed8e128fb788acc31be67
Bzuzinho <ricardo.jgvf@gmail.com>
2026-09-25 09:19:49 +0100

    Adicionar metadados de upload de documentos

---
+++ apps/api/src/order-documents/order-documents.dto.ts
@@ -0,0 +1,28 @@
+import { IsIn, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class UploadOrderDocumentDto {
+  @IsString()
+  @MaxLength(120)
+  label!: string;
+
+  @IsString()
+  @IsIn([
+    'INVOICE',
+    'RECEIPT',
+    'INVOICE_RECEIPT',
+    'CREDIT_NOTE',
+    'DELIVERY_NOTE',
+    'OTHER',
+  ])
+  type!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(120)
+  reference?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(30)
+  documentDate?: string;
+}
